d5d657385237fa5b24202ac4fc1bf770bea33d4e
Karel Picman <karel.picman@kontron.com>
2018-01-16 09:27:14 +0100

    unloadable is no more needed refs Redmine#20513

--- app/models/indexinglog.rb
+++ app/models/indexinglog.rb
@@ -20,7 +20,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 class Indexinglog < ActiveRecord::Base
-  unloadable
 
   belongs_to :repository
   belongs_to :changeset
