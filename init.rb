120556555e706d4cd5f0a5a047c962b19b9151b3
Karel Pičman <karel.picman@kontron.com>
2025-07-07 13:18:20 +0200

    v4.0.4 devel started

--- init.rb
+++ init.rb
@@ -27,7 +27,7 @@ Redmine::Plugin.register :redmine_xapian do
   author_url 'https://github.com/xelkano/redmine_xapian/graphs/contributors'
 
   description 'With this plugin you will be able to do searches by file name and by strings inside your documents'
-  version '4.0.3'
+  version '4.0.4 devel'
   requires_redmine version_or_higher: '6.0.0'
 
   settings partial: 'settings/redmine_xapian_settings',
